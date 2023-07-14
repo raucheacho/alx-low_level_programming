d5c7dedbd25044c077addf0ab4a752ec7f9a3ffe
rauche acho <rauche@live.fr>
2023-07-13 18:55:00 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char);
+	printf("Size of a int: %zu byte(s)\n", sizeof(int);
+	printf("Size of a double: %zu byte(s)\n", sizeof(double));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	return (0);
+}
